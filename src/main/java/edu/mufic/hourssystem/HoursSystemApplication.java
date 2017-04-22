996e4e66269d0b771d40164548be7f9728c91916
tamerrab2003 <tamerrab2003@gmail.com>
2017-04-22 21:35:57 +0200

    HoursSystem new project

---
+++ src/main/java/edu/mufic/hourssystem/HoursSystemApplication.java
@@ -0,0 +1,12 @@
+package edu.mufic.hourssystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication
+public class HoursSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoursSystemApplication.class, args);
+	}
+}
